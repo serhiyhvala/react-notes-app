69c78bfb4fa0deebe324ed1f09f7e48dd27a2805
Your Name <you@example.com>
2022-04-07 13:06:22 +0300

    create new notes component

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,13 +1,18 @@
 import Note from "./Note";
-
-const NotesList = ({ notes }) => {
+import AddNote from "./AddNote";
+const NotesList = ({ notes, handleAddNote }) => {
     return(
         <div className="notes-list">
             {notes.map((note) => (
-            <Note id={note.id} text={note.text} date={note.date}/>
+                <Note 
+                    id={note.id} 
+                    text={note.text} 
+                    date={note.date}
+                />
             ))}
+            <AddNote handleAddNote={handleAddNote}/>
         </div>
-    )
-}
+    );
+};
 
 export default NotesList;
